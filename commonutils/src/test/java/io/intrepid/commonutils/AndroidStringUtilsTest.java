e8b10d3189a2c5e1ac277101f65be2109c8cb7cd
David Zou <david2@intrepid.io>
2017-05-19 14:50:16 -0400

    Remove PowerMock

--- commonutils/src/test/java/io/intrepid/commonutils/AndroidStringUtilsTest.java
+++ commonutils/src/test/java/io/intrepid/commonutils/AndroidStringUtilsTest.java
@@ -1,30 +1,16 @@
 package io.intrepid.commonutils;
 
-import android.text.SpannableStringBuilder;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-
-import io.intrepid.commonutils.util.MockedStringSpannableStringBuilder;
+import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Tests {@link AndroidStringUtils}
  */
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore({ "org.mockito.*", "android.*" })
-@PrepareForTest({ SpannableStringBuilder.class, AndroidStringUtils.class })
+@RunWith(RobolectricTestRunner.class)
 public class AndroidStringUtilsTest {
-
-    @Before
-    public void setup() {
-        MockedStringSpannableStringBuilder.setup();
-    }
 
     @Test
     public void testAppend() throws Exception {
